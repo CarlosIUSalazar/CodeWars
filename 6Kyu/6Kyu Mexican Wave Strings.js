80876825d892d44152163a1dbcb9c6e56f3417e8
Carlos Salazar <carlosiusalazar@gmail.com>
2019-12-16 12:24:58 +0900

    16Dec Mexican Wave 6Kyu Code Challenge solved

--- 6Kyu/6Kyu Mexican Wave Strings.js
+++ 6Kyu/6Kyu Mexican Wave Strings.js
@@ -1,3 +1,4 @@
+//https://www.codewars.com/kata/58f5c63f1e26ecda7e000029/solutions/javascript
 // In this simple Kata your task is to create a function that turns a string into a Mexican Wave. You will be passed a string and you must return that string in an array where an uppercase letter is a person standing up.
 // Rules
 //  	1.  The input string will always be lower case but maybe empty.
