c66e94d854ebf0a2671dd53371b313ca2a4ff990
Carlos Salazar <carlosiusalazar@gmail.com>
2019-12-18 16:08:13 +0900

    Roman To Decimal finished

--- 6Kyu/6Kyu Roman To Decimal.js
+++ 6Kyu/6Kyu Roman To Decimal.js
@@ -59,27 +59,27 @@ if (roman[i]+roman[i+1]=== 'CM') {
 
 console.log("The total is: "+ solution('MDCLXVI'))
 
-// // Advanced Solutions:
-// function solution(roman){
-//     var conversion = {M: 1000, CM: 900, D: 500, CD: 400, C: 100, XC: 90, L: 50, XL: 40, X: 10, IX: 9, V: 5, IV: 4, I: 1};
+// Advanced Solutions:
+function solution(roman){
+    var conversion = {M: 1000, CM: 900, D: 500, CD: 400, C: 100, XC: 90, L: 50, XL: 40, X: 10, IX: 9, V: 5, IV: 4, I: 1};
     
-//     return roman.match(/CM|CD|XC|XL|IX|IV|\w/g).reduce((accum, roman) => accum + conversion[roman], 0);
-//  }
+    return roman.match(/CM|CD|XC|XL|IX|IV|\w/g).reduce((accum, roman) => accum + conversion[roman], 0);
+ }
 
-//  ///////
-//  function solution(roman){
-//     var rom ={ "I":1,"V":5,"X":10,"L":50,"C":100,"D":500,"M":1000};
-//     return roman.split('').reverse().reduce(
-//         function(dec,c,i,rr){ 
-//             c=rom[c];
-//             i=rom[rr[i-1]]||0; 
-//             return dec + (i<=c? c: -c) }
-//         ,0
-//     )
-// }
+ ///////
+ function solution(roman){
+    var rom ={ "I":1,"V":5,"X":10,"L":50,"C":100,"D":500,"M":1000};
+    return roman.split('').reverse().reduce(
+        function(dec,c,i,rr){ 
+            c=rom[c];
+            i=rom[rr[i-1]]||0; 
+            return dec + (i<=c? c: -c) }
+        ,0
+    )
+}
 
-// /////
-// function solution(roman) {
-//     var value = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000};
-//     return roman.split('').map( d => value[d] ).reduce( (s,v,i,o) => s + ( (o[i+1] > v) ? -v : v ), 0 );
-//   }
+/////
+function solution(roman) {
+    var value = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000};
+    return roman.split('').map( d => value[d] ).reduce( (s,v,i,o) => s + ( (o[i+1] > v) ? -v : v ), 0 );
+  }
