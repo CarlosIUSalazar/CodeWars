fb3332f0606329158a122ada4cef800bc2699e69
Carlos Salazar <carlosiusalazar@gmail.com>
2019-12-18 16:13:42 +0900

    Array Difference 6Kyu

--- 6Kyu/6Kyu ArrayDiff.js
+++ 6Kyu/6Kyu ArrayDiff.js
@@ -23,3 +23,20 @@ function array_diff(a, b) {
 }
 
 console.log(array_diff([1,2,3,4,3,4],[3,2]))
+
+// Avanced Solitions
+// function array_diff(a, b) {
+//     return a.filter(function(x) { return b.indexOf(x) == -1; });
+// }
+
+// ///////
+
+
+// function array_diff(a, b) {
+//     return a.filter(e => !b.includes(e));
+//   }
+
+
+//   /////
+
+
