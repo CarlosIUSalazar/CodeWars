a4083c32dc9255e72724c290e7f9eb0a13a44134
Carlos Salazar <carlosiusalazar@gmail.com>
2019-12-18 16:06:30 +0900

    Array Difference 6Kyu

---
+++ 6Kyu/6Kyu ArrayDiff.js
@@ -0,0 +1,25 @@
+function array_diff(a, b) {
+  let exists = false;
+  let newArr = [];
+  if (b.length === 0){  // if (b === []) is false because JS compares 2 objects and are never the same https://stackoverflow.com/questions/35749660/javascript-if-conditions-array-and-array-length-0
+    return a;
+  } 
+
+  for (let i = 0; i<a.length; i++){
+    exists = false;
+    for (let j = 0; j<b.length; j++){  
+    
+      if (a[i] === b[j]){
+        exists = true;
+      } 
+    }
+      if (exists === false){
+        newArr.push(a[i]);
+        //exists = true;
+      }
+  
+}
+  return newArr;
+}
+
+console.log(array_diff([1,2,3,4,3,4],[3,2]))
