c5e26736530eaadf20b79a51da421f8946fb4d07
Carlos Salazar <carlosiusalazar@gmail.com>
2019-12-21 16:00:14 +0900

    Update

--- 6Kyu/6KyuSplitStrings.js
+++ 6Kyu/6KyuSplitStrings.js
@@ -5,33 +5,33 @@
 // solution('abc') // should return ['ab', 'c_']
 // solution('abcdef') // should return ['ab', 'cd', 'ef']
 
-function solution(str){
-    newArr = [];
-    let strAsArr = str.split('');
-    let counter = 0;
-    let newString = '';
+// function solution(str){
+//     newArr = [];
+//     let strAsArr = str.split('');
+//     let counter = 0;
+//     let newString = '';
   
-    for (let i=0; i<str.length; i++){
+//     for (let i=0; i<str.length; i++){
   
-      if (counter < 2) {
-        newString += str[i]
-        counter++;
-        console.log(newString)
-        if (counter === 2){
-          newArr.push(newString);
-          counter = 0;
-          newString = '';
-        }
-      }
-    }
+//       if (counter < 2) {
+//         newString += str[i]
+//         counter++;
+//         console.log(newString)
+//         if (counter === 2){
+//           newArr.push(newString);
+//           counter = 0;
+//           newString = '';
+//         }
+//       }
+//     }
   
-    if (str.length % 2 != 0){
-      newArr.push(str[str.length-1] + '_')
-    }
+//     if (str.length % 2 != 0){
+//       newArr.push(str[str.length-1] + '_')
+//     }
   
-  console.log(strAsArr);
-  return newArr;
+//   console.log(strAsArr);
+//   return newArr;
   
-  }
+//   }
   
   console.log(solution('aslkdfj'));
