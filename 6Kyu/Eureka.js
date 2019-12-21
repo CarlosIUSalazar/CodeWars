20b30f60707aa1bb062c07ce630f7fbddb820205
Carlos Salazar <carlosiusalazar@gmail.com>
2019-12-21 16:02:23 +0900

    Added advanced solutions

--- 6Kyu/Eureka.js
+++ 6Kyu/Eureka.js
@@ -33,3 +33,35 @@ return result;
 }
 
  console.log(sumDigPow(8,200))
+
+ /////  Advanced Solutions
+//  function sumDigPow(a, b) {
+//     var ans = [];
+//     while(a <= b){
+//       if(a.toString().split('').reduce((x,y,i)=>x + +y ** (i + 1),0) == a)
+//         ans.push(a);
+//       a++;
+//     }
+//     return ans;
+//   }
+
+
+///
+////
+// const EUREKAS = [1, 2, 3, 4, 5, 6, 7, 8, 9, 89,135, 175, 518, 598, 1306, 1676, 2427, 2646798];
+
+// const sumDigPow = (a, b) => EUREKAS.filter( (n) => a <= n && n <= b );
+
+
+/////
+// function sumDigPow(a, b) {
+//     var arr = [];
+//     for (var i = a; i <= b; i++) {
+//       var sum = 0;
+//       for (var j = 0; j <= String(i).length; j++) {
+//         sum += Math.pow(parseInt(String(i)[j]), j+1);  
+//         if (sum == i) arr.push(i);
+//       }
+//     }
+//     return arr;
+//   }
