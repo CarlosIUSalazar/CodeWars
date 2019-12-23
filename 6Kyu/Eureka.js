ce74c693a908bf4c653bd73a6dc932d8d99f159a
Carlos Salazar <carlosiusalazar@gmail.com>
2019-12-23 22:09:56 +0900

    Fixed Eureka.js

--- 6Kyu/Eureka.js
+++ 6Kyu/Eureka.js
@@ -37,15 +37,15 @@ return result;
  console.log(sumDigPow(8,200))
 
  /////  Advanced Solutions
-//  function sumDigPow(a, b) {
-//     var ans = [];
-//     while(a <= b){
-//       if(a.toString().split('').reduce((x,y,i)=>x + +y ** (i + 1),0) == a)
-//         ans.push(a);
-//       a++;
-//     }
-//     return ans;
-//   }
+ function sumDigPow(a, b) {
+    var ans = [];
+    while(a <= b){
+      if(a.toString().split('').reduce((x,y,i)=>x + +y ** (i + 1),0) == a)
+        ans.push(a);
+      a++;
+    }
+    return ans;
+  }
 
 
 ///
@@ -56,14 +56,14 @@ return result;
 
 
 /////
-// function sumDigPow(a, b) {
-//     var arr = [];
-//     for (var i = a; i <= b; i++) {
-//       var sum = 0;
-//       for (var j = 0; j <= String(i).length; j++) {
-//         sum += Math.pow(parseInt(String(i)[j]), j+1);  
-//         if (sum == i) arr.push(i);
-//       }
-//     }
-//     return arr;
-//   }
+function sumDigPow(a, b) {
+    var arr = [];
+    for (var i = a; i <= b; i++) {
+      var sum = 0;
+      for (var j = 0; j <= String(i).length; j++) {
+        sum += Math.pow(parseInt(String(i)[j]), j+1);  
+        if (sum == i) arr.push(i);
+      }
+    }
+    return arr;
+  }
