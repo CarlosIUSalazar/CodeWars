fdde91a33844dcd3ca7b991545d9444da3685f61
Carlos Salazar <carlosiusalazar@gmail.com>
2019-12-24 19:20:04 +0900

    Almost there

--- 5Kyu/Rot13.js
+++ 5Kyu/Rot13.js
@@ -1,8 +1,38 @@
-//https://www.codewars.com/kata/530e15517bc88ac656000716/train/javascript
-//ROT13 is a simple letter substitution cipher that replaces a letter with the letter 13 letters after it in the alphabet. ROT13 is an example of the Caesar cipher.
-
-//Create a function that takes a string and returns the string ciphered with Rot13. If there are numbers or special characters included in the string, they should be returned as they are. Only letters from the latin/english alphabet should be shifted, like in the original Rot13 "implementation".
-
+//24 Dec 2019.  5Kyu  Rot 13
+// a(97)   b c d e f(102) g h i j k(107) l m(+13=z) n(110) o p(112) q r s t u(117) v w x y z(122)
+// A(65)   b c d e f(70) g h i j k(75) l m(+13=z) n(78) o p(80) q r s t u(85) v w x y Z(90)
+//String.fromCharCode(72) gives H
+//'H'.charCodeAt(0)
+//debugger;
 function rot13(message){
-  //your code here
-}
+    let temp =''
+    let newString = '';
+    for(let i = 0; i < message.length; i++){
+      temp = message[i].charCodeAt();
+      //console.log(temp);
+      if (temp <65 && temp >122) {
+        if (temp > 90 && temp < 97){
+          newString += temp;
+        }
+      } else if (temp <= 90){
+          temp = temp + 13;
+        if (temp > 90){
+          temp = temp - 25
+          newString += String.fromCharCode(temp);
+        } else if (temp <= 90){
+          newString += String.fromCharCode(temp);
+        }
+      } else if (temp >= 97){
+          temp = temp + 13
+          if (temp > 122){
+            temp = temp - 25
+            newString += String.fromCharCode(temp);
+          } else if (temp <= 122){
+            newString += String.fromCharCode(temp);
+          }
+      }
+    
+    }
+    return newString;
+    
+    }
