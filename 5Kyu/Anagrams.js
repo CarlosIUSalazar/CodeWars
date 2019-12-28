ffb7962af968a6a9573edfd224d62df72bdd6b72
Carlos Salazar <carlosiusalazar@gmail.com>
2019-12-28 12:36:21 +0900

    Working wolution added advanced solutions

--- 5Kyu/Anagrams.js
+++ 5Kyu/Anagrams.js
@@ -25,3 +25,25 @@ return resultArray;
 
 }
 console.log(anagrams('perro',['roepr','rorpe','rompe','mamar']));
+
+
+/////  Advanced solutions
+String.prototype.sort = function() {
+  return this.split("").sort().join("");
+};
+
+function anagrams(word, words) {
+  return words.filter(function(x) {
+      return x.sort() === word.sort();
+  });
+}
+
+
+/////
+function anagrams(word, words) {
+  word = word.split('').sort().join('');
+  return words.filter(function(v) {return word == v.split('').sort().join('');});
+}
+
+/////
+
