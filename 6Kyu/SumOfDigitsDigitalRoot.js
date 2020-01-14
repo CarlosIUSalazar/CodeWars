caedcf152e284189615ae3732b8d4924e93c27ae
Carlos Salazar <carlosiusalazar@gmail.com>
2020-01-14 10:35:54 +0900

    6Kyu Sum Of Digits / Digital Root Kata Advanced Solutions Add

--- 6Kyu/SumOfDigitsDigitalRoot.js
+++ 6Kyu/SumOfDigitsDigitalRoot.js
@@ -23,3 +23,58 @@ function digital_root(n) {
     } 
     return result2
   }
+
+  ///ADVANCED SOLUTIONS  https://www.codewars.com/kata/541c8630095125aba6000c00/solutions/javascript
+  function digital_root(n) {
+    return (n - 1) % 9 + 1;
+  }
+
+////
+  function digital_root(n) {
+    if (n < 10) return n;
+    
+    return digital_root(
+      n.toString().split('').reduce(function(acc, d) { return acc + +d; }, 0));
+  }
+
+////
+  function digital_root(n) {
+    if (n < 10)
+      return n;
+  
+    for (var sum = 0, i = 0, n = String(n); i < n.length; i++)
+      sum += Number(n[i]);
+     
+    return digital_root(sum);
+  }
+
+  ////
+  function digital_root(n){
+    n = eval(n.toString().split('').join('+'));
+
+    if (n > 9) {
+        return digital_root(n);
+    }
+
+    return n;
+}
+
+
+/////
+function digital_root(n) {
+    if (n < 10) return n
+    return digital_root(n % 10 + digital_root(Math.floor(n / 10)))
+  }
+
+
+  ////
+
+  function digital_root(n) {
+    return--n%9+1;
+  }
+
+  ///
+  function digital_root(n) {
+    while (n > 9) { n = (''+n).split('').reduce(function(s,d) {return +s + +d;}); }
+    return n;
+  }
