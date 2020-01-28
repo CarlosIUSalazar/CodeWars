5cd90cc6e49eb8453b627f7a7bfd68cf21d15e48
Carlos Salazar <carlosiusalazar@gmail.com>
2020-01-28 19:12:07 +0900

    6Kyu Primordial

---
+++ 6Kyu/PrimordialOfANumber.js
@@ -0,0 +1,8 @@
+// 6Kyu Definition (Primorial Of a Number)
+Is similar to factorial of a number, In primorial, not all the natural numbers get multiplied, only prime numbers are multiplied to calculate the primorial of a number. It's denoted with P#.
+
+Task
+Given a number N , calculate its primorial. !alt !alt
+
+Notes
+Only positive numbers will be passed (N > 0) .
