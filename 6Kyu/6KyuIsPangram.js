9e12b9955b9887efa617ac350aef9799589c1d45
CarlosIUSalazar <carlosiusalazar@gmail.com>
2020-03-07 19:02:49 +0900

    Added advanced solutions

--- 6Kyu/6KyuIsPangram.js
+++ 6Kyu/6KyuIsPangram.js
@@ -41,4 +41,13 @@ function isPangram(string){
       .every((x) => string.toLowerCase().includes(x));
   }
 
-  
+  /////
+
+  function isPangram(str) {
+    var s = str.toLowerCase();
+    var letters = "zqxjkvbpygfwmucldrhsnioate";
+    for (var i = 0; i < 26; i++)
+        if (s.indexOf(letters.charAt(i)) == -1)
+            return false;
+    return true;
+}
