882dd53c3f828047b448d1994f440897d890e85f
CarlosIUSalazar <carlosiusalazar@gmail.com>
2020-03-07 19:02:32 +0900

    Added advanced solutions

--- 6Kyu/6KyuIsPangram.js
+++ 6Kyu/6KyuIsPangram.js
@@ -32,3 +32,13 @@ function isPangram(string){
   function isPangram(string){
     return (string.match(/([a-z])(?!.*\1)/ig) || []).length === 26;
   }
+
+  //////
+
+  function isPangram(string){
+    return 'abcdefghijklmnopqrstuvwxyz'
+      .split('')
+      .every((x) => string.toLowerCase().includes(x));
+  }
+
+  
