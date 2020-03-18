27a1130510f40d6800236675340030d906f89630
Carlos Salazar <carlosiusalazar@gmail.com>
2020-03-18 12:35:42 +0900

    Passes all tests but needs to do it under 12000ms, needs optimisation

--- 5Kyu/JosephysSurvivorKata.js
+++ 5Kyu/JosephysSurvivorKata.js
@@ -14,35 +14,73 @@
 // The above link about the "base" kata description will give you a more thorough insight about the origin of this kind of permutation, but basically that's all that there is to know to solve this kata.
 
 // Notes and tips: using the solution to the other kata to check your function may be helpful, but as much larger numbers will be used, using an array/list to compute the number of the survivor may be too slow; you may assume that both n and k will always be >=1.
-debugger;
-function josephusSurvivor(n, k) {
-    //your code here
-    let originalArray = [1,2,3,4,5,6,7]
+// debugger;
+// function josephusSurvivor(n, k) {
+//     //your code here
+//     let originalArray = [1,2,3,4,5,6,7]
 
     // for (let i = 1; i <= n; i++) {
     //     originalArray.push(i)
     // }
 
-    console.log(originalArray)
+//     console.log(originalArray)
+//     let temp = 0;
+
+//     let long = (n * k)
+   
+//     for (let i = 0; i <= long; i++){
+//         temp++
+//         if(originalArray.length === 1){
+//             return originalArray[0]
+//         } else {
+//             if (i === originalArray.length){
+//                 i === 0;
+//             }
+//             if (temp === k) {
+//                 originalArray.splice(i,1)
+//                 temp = 0;
+//             }
+//         }
+//     } 
+
+
+// }
+
+
+  //  Different Approach
+//debugger;
+  function josephusSurvivor(n, k) {
+    //your code here
+    let originalArray = []
+
+     for (let i = 1; i <= n; i++) {
+         originalArray.push(i)
+     }
+//console.log(originalArray)
+    let tempArray = [];
+    let long = (n * k)
     let temp = 0;
 
-    let long = (n * k)
-   
-    for (let i = 0; i <= long; i++){
-        temp++
-        if(originalArray.length === 1){
-            return originalArray[0]
-        } else {
-            if (i === originalArray.length){
-                i === 0;
-            }
-            if (temp === k) {
-                originalArray.splice(i,1)
-                temp = 0;
-            }
+    for(let i = 0; i<=long; i++){
+        if(originalArray.length === 1 && tempArray.length === 0){
+            return originalArray[0];
         }
-    } 
+        temp++
+        if(temp !== k){
+            tempArray.push(originalArray[0])
+            originalArray.shift()
+            if(originalArray.length === 0){
+                originalArray = originalArray.concat(tempArray)
+                tempArray = [];
+            }
+        } 
+        if (temp === k) {
+            originalArray.shift()
+            originalArray = originalArray.concat(tempArray)
+            tempArray = [];
+            temp = 0;
+        }
+    }
+  }
 
-
-}
-    console.log(josephusSurvivor(7, 3));
+  console.log(josephusSurvivor(4898,2790));
