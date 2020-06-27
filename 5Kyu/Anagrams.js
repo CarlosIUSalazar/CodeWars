b68dea571238cb7660a225adb7b2180a2ed33439
Carlos Salazar <carlosiusalazar@gmail.com>
2020-06-28 08:22:03 +0900

    refactor

--- 5Kyu/Anagrams.js
+++ 5Kyu/Anagrams.js
@@ -25,7 +25,6 @@ console.log(anagrams('perro',['roepr','rorpe','rompe','mamar']));
 // String.prototype.sort = function() {
 //   return this.split("").sort().join("");
 // };
-
 // function anagrams(word, words) {
 //   return words.filter(function(x) {
 //       return x.sort() === word.sort();
